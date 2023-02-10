7e3f97d4d686008ea1b20d104fbb378bcbfddcd6
Romulo Gomes <romulo.redg@gmail.com>
2023-02-09 22:58:58 -0300

    Setup cucumber - imcomplete

---
+++ src/test/java/br/com/brothertec/springboot/bdd/steps/EmployeeSteps.java
@@ -0,0 +1,24 @@
+package br.com.brothertec.springboot.bdd.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class EmployeeSteps {
+
+    @Dado("some condtion")
+    public void some_condtion() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("do something")
+    public void do_something() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+    @Entao("something happens")
+    public void something_happens() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/resources/features/employee.feature
@@ -0,0 +1,7 @@
+#language: pt
+Funcionalidade:  Fake feature
+
+  Cenario: Test Fake
+    Dado some condtion
+    Quando do something
+    Entao something happens
